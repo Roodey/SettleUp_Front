65691876fd7d46b5da281a785f06f87386da2e74
Roodey <joey8williams@gmail.com>
2017-03-06 06:46:02 -0500

    Still working on storing users. Need a good query method

--- app/routes/login.js
+++ app/routes/login.js
@@ -5,15 +5,35 @@ export default Ember.Route.extend({
 
   actions:{
     login(providerName){
-      console.log(providerName);
         get(this,'session').open('firebase', {provider: providerName}).then(data => {
-          //TODO: SEND THIS DATA TO THE DATABASE SO LOGIN CAN BE STORED
           console.log(data);
-//TODO: IMPLEMENT QUERYRECORD SO WE CAN SEE IF THEY HAVE MADE AN ACCOUNT YET.
-          //var user = this.store.queryRecord('user',data.currentUser.email);
-          //console.log(user);
+          const email = data.currentUser.email;
+          let userCheck;
+//Find out if this is a returning user
+          this.store.query('user',{
+            orderBy: 'email',
+            equalTo: email
+          }).then(foundUsers =>
+            userCheck =  foundUsers.get('firstObject')
+          ).then(user => userCheck = user);
+          console.info(userCheck);
 
-          this.transitionTo('index');
+//If the user was not found, create him
+          if(userCheck === undefined){
+            console.log('Creating new user');
+            const newUser = this.store.createRecord('user',{
+              email: data.currentUser.email,
+              displayName: data.currentUser.displayName
+            });
+            newUser.save();
+          }
+//Delete this later.
+          else{
+            console.log("Welcome returning user!");
+          }
+
+//Once login has been completed, transition to the index route.
+          //this.transitionTo('index');
         });
 
           },
