fff527ecff98016f93596b3d89fbbd7e20218ce2
Roodey <joey8williams@gmail.com>
2017-04-05 16:05:21 -0400

    few bug fixes

--- app/routes/login.js
+++ app/routes/login.js
@@ -12,10 +12,9 @@ export default Ember.Route.extend({
     login(providerName){
         get(this,'session').open('firebase', {provider: providerName}).then(data => {
           console.log(data);
-          let newUser = Model.user;
 //Find out if this is a returning user
         newUser.save().then(response =>{
-          console.log(response)
+          console.log(response);
         }).catch(error => console.error(error));
 
 //Once login has been completed, transition to the index route.
